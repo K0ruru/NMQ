df2e7e8d4bafefd618adef47d206746322f01f04
K0ruru <nurfaizqalbi@gmail.com>
2023-07-24 17:46:44 +0700

    Tugas1
    cool right

---
+++ Tugas1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css-files\Tugas1.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <table>
+    <!-- //membuat row/baris -->
+    <tr>
+      <!-- //membuat cell -->
+      <td>Beranda</td>
+      <td>Tutorial</td>
+      <td>Nyekrip</td>
+      <td>Tentang</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td>PHP</td>
+
+    </tr>
+    <tr>
+      <td></td>
+      <td>CSS</td>
+
+    </tr>
+    <tr>
+      <td></td>
+      <td>HTML</td>
+
+    </tr>
+  </table>
+  </div>
+</body>
+
+</html>
